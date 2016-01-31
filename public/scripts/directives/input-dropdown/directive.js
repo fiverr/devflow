b7a21f280c2d8657ac574f3aa0f9f5dc3c808201
Arkady Fukzon <arkady.f@fiverr.com>
2016-01-31 10:19:01 +0200

    [FEATURE] input dropdown arrow keys selection

--- public/scripts/directives/input-dropdown/directive.js
+++ public/scripts/directives/input-dropdown/directive.js
@@ -11,6 +11,7 @@ devflowApp.directive('inputDropdown', ['$timeout', function($timeout) {
         controller: function($scope) {
             $scope.displayItems = [];
             $scope.inputFocused = false;
+            $scope.focusedIndex = -1;
 
             var setSelectedText = function() {
                 $scope.selectedText = $scope.selectedItem[$scope.displayProperty];
@@ -24,11 +25,13 @@ devflowApp.directive('inputDropdown', ['$timeout', function($timeout) {
                 $scope.selectedItem = item;
                 setSelectedText();
                 $scope.displayItems = [];
+                $scope.focusedIndex = -1;
             }
 
             $scope.itemTextChanged = function() {
                 $scope.selectedItem = null;
                 $scope.displayItems = [];
+                $scope.focusedIndex = -1;
 
                 if ($scope.selectedText && $scope.selectedText != '') {
                     for (i = 0; i < $scope.items.length; i++) {
@@ -42,11 +45,30 @@ devflowApp.directive('inputDropdown', ['$timeout', function($timeout) {
             $scope.inputBlur = function() {
                 $timeout(function() {
                     $scope.inputFocused = false;
+                    $scope.focusedIndex = -1;
                 }, 100);
             }
 
             $scope.inputFocus = function() {
                 $scope.inputFocused  = true;
+            }
+
+            $scope.keyDown = function(keyEvent) {
+                if (!($scope.displayItems.length > 0 && $scope.selectedText.length > 0 && $scope.inputFocused)) {
+                    return;
+                }
+
+                if (keyEvent.keyCode == 40 && $scope.focusedIndex < $scope.displayItems.length - 1) { // down arrow
+                    $scope.focusedIndex++;
+                } else if (keyEvent.keyCode == 38 && $scope.focusedIndex > 0) { // up arrow
+                    $scope.focusedIndex--;
+                } else if (keyEvent.keyCode == 13) { // enter
+                    $scope.selectItem($scope.displayItems[$scope.focusedIndex]);
+                }
+            }
+
+            $scope.isFocused = function(item) {
+                return ($scope.focusedIndex == $scope.displayItems.indexOf(item));
             }
         }
     }
