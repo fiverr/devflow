b44423a9d7092258877fad16c07cde955103c4ed
Arkady Fukzon <arkady.f@fiverr.com>
2016-02-08 12:28:59 +0200

    [FEATURE] add js compress on boot, enable express connect compress(gzip)

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
+app.use(express.compress());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(express.cookieParser());
@@ -32,6 +33,9 @@ app.use(app.router);
 
 // connect to db
 mongoose.connect(config.mongo);
+
+// concat and uglify
+require('./compress').compress(env);
 
 // setup sass middleware
 app.use(
---
+++ compress.js
@@ -0,0 +1,41 @@
+var compressor = require('node-minify'),
+    files = [
+        'public/scripts/app.js',
+        'public/scripts/config.js',
+        'public/scripts/services/userService.js',
+        'public/scripts/services/notificationService.js',
+        'public/scripts/services/requestService.js',
+        'public/scripts/services/serverService.js',
+        'public/scripts/services/reviewService.js',
+        'public/scripts/services/repoService.js',
+        'public/scripts/services/tagService.js',
+        'public/scripts/controllers/mainController.js',
+        'public/scripts/controllers/dashboardController.js',
+        'public/scripts/controllers/serverController.js',
+        'public/scripts/controllers/userController.js',
+        'public/scripts/controllers/requestController.js',
+        'public/scripts/controllers/manageServersController.js',
+        'public/scripts/controllers/reposController.js',
+        'public/scripts/controllers/tagController.js',
+        'public/scripts/directives/ngSrc.js',
+        'public/scripts/directives/request/directive.js',
+        'public/scripts/directives/review/directive.js',
+        'public/scripts/directives/server-env/directive.js',
+        'public/scripts/directives/server/directive.js',
+        'public/scripts/directives/model-manager/directive.js',
+        'public/scripts/directives/input-dropdown/directive.js'
+    ];
+
+module.exports = {
+    compress: function(env) {
+        new compressor.minify({
+            type: 'uglifyjs',
+            fileIn: files,
+            fileOut: 'public/scripts/application.js',
+            options: ['--compress'],
+            callback: function(err, min) {
+                console.log('minified and uglified js files');
+            }
+        });
+    }
+};
