9b72f49d79a91d0c9259daf2385ac397d58a659c
Oron Riff <oron.riff@fiverr.com>
2016-02-14 12:49:04 +0200

    [App Manager script] Upload new app manger script

---
+++ control.sh
@@ -0,0 +1,47 @@
+start ()
+{
+	startAppCorrectly
+}
+
+stop ()
+{
+	npm stop
+}
+
+restart ()
+{
+	startAppCorrectly
+}
+
+startAppCorrectly ()
+{
+	npm install
+    	if [ $? -ne "0" ]
+    	then
+    		echo "${APP} is down , starting..."
+        	npm start
+    	else
+    		npm run-script deploy
+	fi
+}
+
+# MAIN
+
+CURR_DIR=`pwd`
+
+action=$1
+export NODE_ENV=$2
+export SHARED_APP_DIR="../../shared/"
+export APP_CWD="/home/admin/apps/fiverr_devflow/current"
+
+case "$action" in
+	stop)
+		stop
+	;;
+	start)
+		start
+	;;
+	restart)
+		restart
+	;;
+esac
